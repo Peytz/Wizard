904db533341ef3b7ddb5ceaab9ec7b0235cdfdc1
Henrik Bjørnskov <henrik@bearwoods.dk>
2011-11-11 17:50:41 +0100

    Apigen generation script

---
+++ apigen.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ORIGINAL_BRANCH_NAME=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+
+# Stash all saved work if that exists
+git stash --incude-untracked --all save
+git checkout master
+
+# Generate and commit apigen
+apigen --source src --destination doc
+git commit -m 'Update ApiGen' doc
+
+# Save last commit so we can cherry-pick it later
+LAST_COMMIT_HASH=$(git rev-parse HEAD)
+
+# Switch branch and cherry pick
+git checkout gh-pages
+git cherry-pick $LAST_COMMIT_HASH
+
+# Go back to previous branch and pop stash
+git checkout $ORIGINAL_BRANCH_BANE
+git stash pop
+
+echo "Update ApiGen in doc/ based on master branch. Also cherry-picked it to gh-pages.\n"
+echo "You should properly push gh-pages"
