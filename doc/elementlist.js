692f161cc37ace4da6e40e8077fb3dee9109ec6c
Henrik Bjørnskov <henrik@bearwoods.dk>
2011-11-16 13:39:58 +0100

    Update ApiGen

--- doc/elementlist.js
+++ doc/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["class","ArrayAccess"],["class","ArrayIterator"],["class","Countable"],["class","Iterator"],["class","IteratorAggregate"],["class","Peytz\\Wizard\\ReportInterface"],["class","Peytz\\Wizard\\Step"],["class","Peytz\\Wizard\\StepInterface"],["class","Peytz\\Wizard\\Wizard"],["class","SeekableIterator"],["class","Serializable"],["class","Traversable"]];
+ApiGen.elements = [["class","Peytz\\Wizard\\ReportInterface"],["class","Peytz\\Wizard\\Step"],["class","Peytz\\Wizard\\StepInterface"],["class","Peytz\\Wizard\\Wizard"]];
