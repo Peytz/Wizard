9744f36edb96f238ce8b4259d0893d2761862fb8
Henrik Bjørnskov <henrik@bjrnskov.dk>
2011-12-22 10:27:47 +0100

    Add build.sh to build a phar archive

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+FILE=$(mktemp)
+OUTPUT="build/peytz-wizard.php"
+SOURCE="src"
+
+STUB="
+<?php
+Phar::mapPhar();
+
+spl_autoload_register(function (\$className) {
+    if (0 !== strpos(\$className, 'Peytz\\\Wizard\\\')) {
+        return false;
+    }
+
+    \$file = 'phar://' . __FILE__ . '/' . str_replace('\\\', DIRECTORY_SEPARATOR, \$className) . '.php';
+
+    if (file_exists(\$file)) {
+        require \$file;
+        return true;
+    }
+
+    return false;
+});
+
+__HALT_COMPILER();
+"
+
+# Generate PHAR file
+echo $STUB > $FILE
+phar-build --ns --src=$SOURCE --phar=$OUTPUT --stub=$FILE
+rm $FILE
